b2aa71ceff80e09b04bbb8abedc2be678659e067
Zik-Tech <111295757+Zik-Tech@users.noreply.github.com>
2022-10-16 23:45:35 +0100

    more features added
    I added some more features like deposit withdrawal bank details logout and did some changes in the login function for better functionalities.But it's still under developement

--- Izik-Bank.py
+++ Izik-Bank.py
@@ -23,7 +23,6 @@ def register():
 
     account_number = randrange(1111111111, 9999999999)
     pin = input("Create a 'Five' digit pin for transaction: ")
-    print(len(pin))
     # to check if the pin created is 5 digit
     while len(pin) < 5 or len(pin) > 5:
         print("Your pin is not of required standard")
@@ -31,26 +30,41 @@ def register():
     pin = int(pin)
 
     print(
-        f"Hello {first_name} {lastname} you have successfully created an account with us.\nYour account number is {account_number}\nYour account number would be required for all bank operations so Keep it safe.\n\n")
+        f"Hello {first_name} {lastname} you have successfully created an account with us."
+        f"\nYour account number is {account_number}"
+        f"\nYour account number would be required for all bank operations so Keep it safe.\n\n")
+
     # a serial num for identification in our data-base
     global serial_num
     serial_num = randrange(222222222222222, 999999999999999)
     database [serial_num] = [first_name, lastname, dob, email, city, state, country, password, re_password,
                              account_number, pin]
-    print(database[serial_num][3])
+    # redirects to log in function
     log_in(database)
 
 
 # The log in function
 def log_in(database):
     print(f"\n\t\t{d} LOG-IN {d}")
-    #print(database.items([serial_num][3]))
-    # login = input("Input account number or email: ")
-    # while login != database[serial_num][3] or database.values([serrial])
+
+    # receive email for log in
+    login = input("Input your email: ")
+    # check if eail is correct
+    while login != (database [serial_num] [3]):
+        print("Incorrect Email.\nTry again")
+        login = input("Input Email: ")
+
+    # receive password
+    loginpassword = input("Input your password: ")
+    # check if password is correct
+    while loginpassword != (database [serial_num] [-3]):
+        print("password is incorrect!")
+        loginpassword = input("Input your password: ")
+    bank_operations(database)
 
 
-# The main banking function
-def bank_operation():
+# The main banking startup page
+def bank_startup():
     print(f"\n\t\t{d} ZIK-BANK {d}")
     acc_option = int(
         input("Welcome to this amazing mobile banking app.\n1.)Create account\n2.)Log in\n\tReply with 1 or 2 > "))
@@ -60,8 +74,59 @@ def bank_operation():
         log_in(database)
     else:
         print("Invalid input")
-        bank_operation()
+        bank_startup()
+
+
+# Bank operations
+def bank_operations(database):
+    print(f"Hello {database [serial_num] [0]} {database [serial_num] [1]} welcome to ZIK-BANK.\n ")
+    operation = input(f"what would you like to do today:"
+                      f"\n1.)Deposit\n2.)Withdraw\n3.)Check Account balance"
+                      f"\n4.)Log-Out\n5.)Exit\n\tReply: ")
+    operation = int(operation)
+    if operation == 1:
+        deposit()
+    elif operation == 2:
+        withdraw()
+    elif operation == 3:
+        details(database)
+    elif operation == 4:
+        log_in(database)
+    elif operation == 5:
+        exit()
+    else:
+        print("invalid option choosen")
+        bank_operations(database)
+
+
+def deposit():
+    print(f"{d} Deposit {d}")
+
+
+def withdraw():
+    print(f"{d} Withdraw {d}")
+
+
+def details(database):
+    print(f"{d} Your Bank Account Details {d}")
+    print(f"Name - {database [serial_num] [0]}-{database [serial_num] [1]}")
+    print(f"Date Of Birth - {database [serial_num] [2]}")
+    print(f"Email address - {database [serial_num] [3]}")
+    print(f"Address - {database [serial_num] [4]},{database [serial_num] [5]},{database [serial_num] [6]}")
+    print(f"Password - {database [serial_num] [7]}")
+    print(f"Account Number - {database [serial_num] [9]}")
+    print(f"Pin - *****")
+    print()
+    opt = input("would you like to check other bank operations 1.) Yes 2.) No : ")
+    int(opt)
+    if opt == 1:
+        bank_operations(database)
+    elif opt == 2:
+        log_in(database)
+    else:
+        print("Invalid input")
+        log_in(database)
 
 
 # THE BANK MAIN CALL #
-bank_operation()
+bank_startup()
